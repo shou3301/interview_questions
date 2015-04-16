1f24d5705ec56021bdb7c70bd56f1a4d33a1c549
Chen <shou3301@gmail.com>
2015-04-15 20:28:12 -0700

    fixed a bug

--- src/elements/dp/sequential_cover.java
+++ src/elements/dp/sequential_cover.java
@@ -5,39 +5,36 @@
  
 class Solution {
     
-    public String smallestCover(String S, String T) {
+    public String minSequtentialWindow(String S, String T) {
         if (S == null || T == null || S.length() == 0 || T.length() == 0) return "";
         
-        // The most recent appreance of T[i] in S
+        Map<Character, Integer> index = new HashMap<>();
         int[] latest = new int[T.length()];
-        // The shortest length of the subarray in S that covers T[0...i]
-        int[] size = new int[T.length()];
+        int[] minLen = new int[T.length()];
         
         Arrays.fill(latest, -1);
-        Arrays.fill(size, Integer.MAX_VALUE);
-        
-        Map<Character, Integer> index = new HashMap<>();
+        Arrays.fill(minLen, Integer.MAX_VALUE);
         for (int i = 0; i < T.length(); i++) {
             index.put(T.charAt(i), i);
         }
         
-        int min = Integer.MAX_VALUE;
         String res = "";
+        int resLen = Integer.MAX_VALUE;
         for (int i = 0; i < S.length(); i++) {
             if (index.containsKey(S.charAt(i))) {
                 int idx = index.get(S.charAt(i));
                 if (idx == 0) {
-                    size[0] = 1;
-                } else if (size[idx - 1] != Integer.MAX_VALUE) {
-                    size[idx] = i - latest[idx - 1] + size[idx - 1];
+                    minLen[idx] = 1;
+                } else if (minLen[idx - 1] != Integer.MAX_VALUE) {
+                    minLen[idx] = i - latest[idx - 1] + minLen[idx - 1];
                 }
-            }
-            
-            latest[idx] = i;
-            
-            if (idx == T.length() - 1 && size[idx] < min) {
-                min = size[idx];
-                res = S.substring(latest[idx] - min + 1, latest[idx] + 1);
+                
+                latest[idx] = i;
+                
+                if (idx == T.length() - 1 && minLen[idx] < resLen) {
+                    resLen = minLen[idx];
+                    res = S.substring(i - resLen + 1, i + 1);
+                }
             }
         }
         
